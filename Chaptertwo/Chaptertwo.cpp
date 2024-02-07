4b594f321a3d4d9b74ca3c683f7deea7875825dc
WyattD21 <157396023+WyattD21@users.noreply.github.com>
2024-02-07 08:25:32 -0500

    Update Chaptertwo.cpp

--- Chaptertwo/Chaptertwo.cpp
+++ Chaptertwo/Chaptertwo.cpp
@@ -1,20 +1,26 @@
-// Chaptertwo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   double originalPrice, markupPercentage, salesTaxRate;
+
+   cout << "Enter the original price of the item: $";
+   cin >> originalPrice;
+   cout << "Enter the markup percentage(Excluding %): ";
+   cin >> markupPercentage;
+   cout << "Enter the sales tax rate(Excluding %): ";
+   cin >> salesTaxRate;
+
+   
+   double listedPrice = originalPrice * (1 + markupPercentage / 100);
+   double salesTax = listedPrice * (salesTaxRate / 100);
+   double total = listedPrice + salesTax;
+
+   cout << endl << "Orignal Price of the item:$" << originalPrice << endl;
+   cout << endl << "Markup Percentage: " << markupPercentage << "%" << endl;
+   cout << endl << "The listed price: " << listedPrice << endl;
+   cout << endl << "Sales tax: $" << salesTax << endl;
+   cout << endl << "Customer pays: $" << total << endl;
+   return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
